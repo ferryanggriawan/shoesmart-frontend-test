58f34ed601fe59e82d60d4d8553c93e94e5a0751
Ferry Anggriawan <ferrya1997@gmail.com>
2021-02-25 23:58:10 +0700

    init

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,21 @@
-import '../styles/globals.css'
+import App from "next/app"
+import { Provider } from "react-redux"
+import configureAppStore from "../lib/store/store"
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import "../styles/globals.css"
+
+const store = configureAppStore()
+
+class MyApp extends App {
+  render() {
+    const { Component, pageProps } = this.props
+
+    return (
+      <Provider store={store}>
+        <Component {...pageProps}></Component>
+      </Provider>
+    )
+  }
 }
 
 export default MyApp
---
+++ pages/index.jsx
@@ -0,0 +1,103 @@
+import Head from "next/head"
+import { withRouter } from "next/router"
+import { Component } from "react"
+import { connect } from "react-redux"
+import styles from "../styles/Home.module.css"
+
+function x() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("data")
+    }, 1000)
+  })
+}
+
+export async function getServerSideProps(context) {
+  let data = "ferry"
+
+  data = await x()
+
+  return {
+    props: {
+      name: data,
+      data: "asd",
+    },
+  }
+}
+
+class Home extends Component {
+  componentDidMount() {
+    console.log(this.props)
+  }
+
+  render() {
+    return (
+      <div className={styles.container}>
+        <Head>
+          <title>Shoesmart: Toko Sepatu</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+
+        <main className={styles.main}>
+          <h1 className={styles.title}>
+            Welcome to <a href="https://nextjs.org">Next.js!</a>
+          </h1>
+
+          <p className={styles.description}>
+            Get started by editing{" "}
+            <code className={styles.code}>pages/index.js</code>
+          </p>
+
+          <div className={styles.grid}>
+            <a href="https://nextjs.org/docs" className={styles.card}>
+              <h3>Documentation &rarr;</h3>
+              <p>Find in-depth information about Next.js features and API.</p>
+            </a>
+
+            <a href="https://nextjs.org/learn" className={styles.card}>
+              <h3>Learn &rarr;</h3>
+              <p>Learn about Next.js in an interactive course with quizzes!</p>
+            </a>
+
+            <a
+              href="https://github.com/vercel/next.js/tree/master/examples"
+              className={styles.card}
+            >
+              <h3>Examples &rarr;</h3>
+              <p>Discover and deploy boilerplate example Next.js projects.</p>
+            </a>
+
+            <a
+              href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+              className={styles.card}
+            >
+              <h3>Deploy &rarr;</h3>
+              <p>
+                Instantly deploy your Next.js site to a public URL with Vercel.
+              </p>
+            </a>
+          </div>
+        </main>
+
+        <footer className={styles.footer}>
+          <a
+            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Powered by{" "}
+            <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+          </a>
+        </footer>
+      </div>
+    )
+  }
+}
+
+const mapState = (state) => ({
+  products: state.product.products,
+})
+
+const mapDispatch = {}
+
+export default withRouter(connect(mapState, mapDispatch)(Home))
